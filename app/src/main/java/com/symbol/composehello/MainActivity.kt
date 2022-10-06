6047ad545202b6b4fee120964ec252f21be59c9f
symbol <15903648970@163.com>
2022-10-06 12:01:43 +0800

    1. 搭建框架 2. 提交文本代码

--- app/src/main/java/com/symbol/composehello/MainActivity.kt
+++ app/src/main/java/com/symbol/composehello/MainActivity.kt
@@ -3,11 +3,22 @@ package com.symbol.composehello
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.Crossfade
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.BottomNavigationItem
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -35,11 +46,147 @@ fun DefaultPreview() {
 
 @Composable
 fun MainHomeScreen() {
-    val clickState = remember { mutableStateOf(BottomNavType.TYPE_HOME) }
+    val bottomSwitchState = remember { mutableStateOf(BottomNavType.TYPE_HOME) }
     ComposeHelloTheme {
-        MainBottomNav(state = clickState)
+        Column {
+            ContentArea(
+                bottomSwitchState.value,
+                modifier = Modifier.weight(1f)
+            )
+            MainBottomNav(state = bottomSwitchState)
+        }
     }
 }
+
+
+@Composable
+fun ContentArea(type: BottomNavType, modifier: Modifier) {
+    Column(modifier = modifier) {
+        Crossfade(type) { content ->
+            when (content) {
+                BottomNavType.TYPE_HOME -> HomeScreen()
+                BottomNavType.TYPE_MESSAGE_TYPE -> MessageScreen()
+                BottomNavType.TYPE_NEWS -> NewsScreen()
+                BottomNavType.TYPE_MINE -> MineScreen()
+            }
+        }
+    }
+
+}
+
+
+/**
+ * 主页
+ */
+@Composable
+fun HomeScreen() {
+    Column(modifier = Modifier.padding(16.dp)) {
+        Text(
+            text = "1. 文本组件",
+            color = Color.Black,
+            textAlign = TextAlign.Start,
+            fontStyle = FontStyle.Normal,
+            fontWeight = FontWeight.Black,
+            modifier = Modifier.padding(vertical = 4.dp)
+        )
+        Text(
+            text = "FontWeight.Light",
+            color = Color.LightGray,
+            fontWeight = FontWeight.Light
+        )
+
+        Text(
+            text = "FontWeight.Black",
+            fontWeight = FontWeight.Black
+        )
+        Text(
+            text = "FontWeight.ExtraBold",
+            fontWeight = FontWeight.ExtraBold
+        )
+        Text(
+            text = "FontWeight.SemiBold",
+            fontWeight = FontWeight.SemiBold
+        )
+        Text(
+            text = "FontWeight.Medium",
+            fontWeight = FontWeight.Medium
+        )
+        Text(
+            text = "FontWeight.Thin",
+            fontWeight = FontWeight.Thin
+        )
+        Text(
+            text = "FontWeight.w100",
+            fontWeight = FontWeight.W100
+        )
+        Text(
+            text = "FontWeight.w900",
+            fontWeight = FontWeight.W900
+        )
+
+        Text(
+            text = "FontFamily.Cursive",
+            fontFamily = FontFamily.Cursive
+        )
+        Text(
+            text = "FontFamily.Monospace",
+            fontFamily = FontFamily.Monospace
+        )
+        Text(
+            text = stringResource(id = R.string.app_name),
+            textDecoration = TextDecoration.LineThrough + TextDecoration.Underline
+        )
+        Text(
+            text = """
+            白日依山尽，
+            黄河入海流。
+            欲穷千里目，
+            更上一层楼。
+        """.trimMargin(),
+            maxLines = 2
+        )
+        Text(
+            text = "千山鸟飞绝，万里人踪灭".repeat(6),
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis,
+        )
+
+        Text(
+            text = "TextOverflow".repeat(6),
+            overflow = TextOverflow.Clip,
+            maxLines = 1
+        )
+
+
+    }
+
+}
+
+
+/**
+ * 消息界面
+ */
+@Composable
+fun MessageScreen() {
+
+}
+
+/**
+ * 消息界面
+ */
+@Composable
+fun NewsScreen() {
+
+}
+
+/**
+ * 我的界面
+ */
+@Composable
+fun MineScreen() {
+
+}
+
 
 @Composable
 fun MainBottomNav(state: MutableState<BottomNavType>) {
